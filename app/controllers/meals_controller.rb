0e1f9b516ed8b03a01609709691945737ba3ebf9
Raghu Betina <raghu@firstdraft.com>
2019-05-30 04:58:53 +0000

    Generate meals resource with draft:resource

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,61 @@
+class MealsController < ApplicationController
+  def index
+    @meals = Meal.all
+
+    render("meal_templates/index.html.erb")
+  end
+
+  def show
+    @meal = Meal.find(params.fetch("id_to_display"))
+
+    render("meal_templates/show.html.erb")
+  end
+
+  def new_form
+    @meal = Meal.new
+
+    render("meal_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @meal = Meal.new
+
+    @meal.eater = params.fetch("eater")
+
+    if @meal.valid?
+      @meal.save
+
+      redirect_back(:fallback_location => "/meals", :notice => "Meal created successfully.")
+    else
+      render("meal_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @meal = Meal.find(params.fetch("prefill_with_id"))
+
+    render("meal_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @meal = Meal.find(params.fetch("id_to_modify"))
+
+    @meal.eater = params.fetch("eater")
+
+    if @meal.valid?
+      @meal.save
+
+      redirect_to("/meals/#{@meal.id}", :notice => "Meal updated successfully.")
+    else
+      render("meal_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @meal = Meal.find(params.fetch("id_to_remove"))
+
+    @meal.destroy
+
+    redirect_to("/meals", :notice => "Meal deleted successfully.")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Meal resource:
+
+  # CREATE
+  get("/meals/new", { :controller => "meals", :action => "new_form" })
+  post("/create_meal", { :controller => "meals", :action => "create_row" })
+
+  # READ
+  get("/meals", { :controller => "meals", :action => "index" })
+  get("/meals/:id_to_display", { :controller => "meals", :action => "show" })
+
+  # UPDATE
+  get("/meals/:prefill_with_id/edit", { :controller => "meals", :action => "edit_form" })
+  post("/update_meal/:id_to_modify", { :controller => "meals", :action => "update_row" })
+
+  # DELETE
+  get("/delete_meal/:id_to_remove", { :controller => "meals", :action => "destroy_row" })
+
+  #------------------------------
+
   # Routes for the Recipe resource:
 
   # CREATE
