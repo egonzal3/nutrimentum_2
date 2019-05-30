91d1234c22c10d29740624c903c3ff55ff3f4865
Raghu Betina <raghu@firstdraft.com>
2019-05-30 04:59:16 +0000

    Whitelist columns in Recipe for ActiveAdmin

--- app/admin/recipes.rb
+++ app/admin/recipes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Recipe do
+
+ permit_params :ingredient1, :ing1_quant, :ing1_metr, :ingredient2, :ing2_quant, :ing2_metr, :ingredient3, :ing3_quant, :ing3_metr, :ingredient4, :ing4_quant, :ing4_metr, :ingredient5, :ing5_quant, :ing5_metr, :total_calories, :total_proteins, :total_fats, :total_carbohydrates
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
